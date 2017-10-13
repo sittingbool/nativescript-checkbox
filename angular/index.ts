51d1b2dee8aaca7894c341c75d63137ad072e81e
Richard Habermann <sittingbool@gmx.de>
2017-10-13 12:12:24 +0200

    fixes #67
    tested in my app with nativescript-angular 4.4.0, but not sure if downwards compatible, would resolve #67

--- angular/index.ts
+++ angular/index.ts
@@ -1,7 +1,7 @@
 import { Directive, ElementRef, HostListener, Inject, NgModule, forwardRef } from "@angular/core";
 import { FormsModule, NG_VALUE_ACCESSOR } from "@angular/forms";
 import { registerElement } from "nativescript-angular/element-registry";
-import { BaseValueAccessor } from "nativescript-angular/value-accessors/base-value-accessor";
+import { BaseValueAccessor } from "nativescript-angular/forms/value-accessors/base-value-accessor";
 import { convertToInt } from "nativescript-angular/common/utils";
 import { View } from "tns-core-modules/ui/core/view";
 
